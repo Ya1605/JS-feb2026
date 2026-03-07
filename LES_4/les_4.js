fc04b86c1d2bee9b4e08587f417c462a944319e9
yuliia <yuliiadev@ukr.net>
2026-03-08 00:19:24 +0200

    30

--- LES_4/les_4.js
+++ LES_4/les_4.js
@@ -1,10 +1,5 @@
 
 
-
-
-
-
-// створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 // створити функцію яка повертає найменьше число з масиву
 // створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 // створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
@@ -108,3 +103,29 @@ function createEl(array) {
 const firstArray = [1, true, 'hello', 45];
 const listHtml = createEl(firstArray);
 document.write(listHtml);
+
+// створити функцію яка приймає масив об'єктів з наступними полями id,
+// name,age ,
+//  та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+ let users = [
+        {name: 'vasya', age: 31, status: false},
+        {name: 'petya', age: 30, status: true},
+        {name: 'kolya', age: 29, status: true},
+        {name: 'olya', age: 28, status: false},
+        {name: 'max', age: 30, status: true},
+        {name: 'anya', age: 31, status: false},
+        {name: 'oleg', age: 28, status: false},
+        {name: 'andrey', age: 29, status: true},
+        {name: 'masha', age: 30, status: true},
+        {name: 'olya', age: 31, status: false},
+        {name: 'max', age: 31, status: true}
+];
+    
+function allUsers(array) {
+    for (const element of array) {
+        console.log(element);
+    }
+}
+
+allUsers(users);
