99100d1ba70b10eb065e04d51bf8470d57b7249f
yuliia <yuliiadev@ukr.net>
2026-03-08 22:45:40 +0200

    48

--- LES_4/les_4.js
+++ LES_4/les_4.js
@@ -1,9 +1,4 @@
 
-
-
-
-// створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
-//риклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
 // Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 //риклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
 //
@@ -212,3 +207,17 @@ function secondMax(array) {
 }
 
 console.log(secondMax([3, 7, 2, 9, 5]));
+
+// створити функцію swap(arr,index1,index2).
+//  Функція міняє місцями заняення у відаовідних індексах
+//риклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+
+function swap(arr,index1,index2) {
+    let temp = [index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+    return arr;
+
+}
+
+console.log(swap([11, 22, 33, 44], 0, 1));
