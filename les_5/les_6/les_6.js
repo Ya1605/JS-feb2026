4abda95120a8b8dc534b6aa157d8354261bac422
yuliia <yuliiadev@ukr.net>
2026-03-16 20:47:22 +0200

    91

--- les_5/les_6/les_6.js
+++ les_5/les_6/les_6.js
@@ -51,7 +51,7 @@ console.log(string);
 
 const secNum = [10, 20, 30];
 const secNum2 = secNum.push("gggg");
-console.log(secNum2);
+console.log(secNum);
 
 //- створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
 //let nums = [11,21,3];
