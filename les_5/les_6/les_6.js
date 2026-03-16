945caa509378c1e0530ac0327e632ed0405d8ca8
yuliia <yuliiadev@ukr.net>
2026-03-16 21:49:45 +0200

    95

--- les_5/les_6/les_6.js
+++ les_5/les_6/les_6.js
@@ -78,6 +78,9 @@ const nums2 = (arr, direction) => arr.sort((a, b) => direction === "dddd" ? a - 
 
 let nums3 = [111, 211, 355];
 console.log(nums2([...nums3], "ccc"));
+
+
+
 //==========================
 //- є масив
 //let coursesAndDurationArray = [
@@ -92,18 +95,17 @@ console.log(nums2([...nums3], "ccc"));
  //-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
  //-- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
  //=========================
- //описати колоду карт (від 6 до туза без джокерів)
- //- знайти піковий туз
- //- всі шістки
- //- всі червоні карти
- //- всі буби
- //- всі трефи від 9 та більше
+ let coursesAndDurationArray = [
+   {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 
-//{
-  //  cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
-   // value: '', // '6'-'10', 'ace','jack','queen','king','joker'
-    //color:'', // 'red','black'
-//}
+const sortCourse = (arr) => arr.sort((a, b) => a - b);
+console.log(sortCourse([...coursesAndDurationArray]));
 
 //=========================
 
