dac787598d0e62fc791335c8ba89c7161cb65bbc
yuliia <yuliiadev@ukr.net>
2026-06-10 23:20:50 +0300

    les_6 -all

--- les_6/les_6.js
+++ les_6/les_6.js
@@ -9,20 +9,42 @@ console.log(arrNum);
 let nums = [11,21,3];
 let sortNums = nums.sort((a,b) => b-a);
 console.log(sortNums);
+console.log(nums);
 //sortNums(nums,'ascending') // [3,11,21]
 //sortNums(nums,'descending') // [21,11,3]
 
 
 //==========================
 //є масив
-//let coursesAndDurationArray = [
-//    {title: 'JavaScript Complex', monthDuration: 5},
- //   {title: 'Java Complex', monthDuration: 6},
-  //  {title: 'Python Complex', monthDuration: 6},
-   // {title: 'QA Complex', monthDuration: 4},
-   // {title: 'FullStack', monthDuration: 7},
-   // {title: 'Frontend', monthDuration: 4}
-//];
+let coursesAndDurationArray = [
+   {title: 'JavaScript Complex', monthDuration: 5},
+   {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+let sortCourse = coursesAndDurationArray.sort((a, b) => a.monthDuration - b.monthDuration);
+console.log(sortCourse);
+let sortNextCourse = coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
+console.log(coursesAndDurationArray);
+ let filterCourse = coursesAndDurationArray.filter(item => item.monthDuration > 5);
+ console.log(filterCourse);
+
  // відсортувати його за спаданням за monthDuration
  //відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
  //за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+let coursesAndDurationArrayy = [
+   {title: 'JavaScript Complex', monthDuration: 5},
+   {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+ let addIndex = coursesAndDurationArrayy.map((item,index) => {
+ return {...item, id: index +1};
+ });
+
+ console.log(addIndex);
+ console.log(coursesAndDurationArrayy);
